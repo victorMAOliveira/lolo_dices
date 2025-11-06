c6af95d38e8c02417df9cb6c895775e5a149a59b
Victor Oliveira <victor.moura.amaral.oliveira@gmail.com>
2025-11-06 15:32:39 -0300

    Basic start for the dice class

---
+++ src/dice/dice_types.py
@@ -0,0 +1,36 @@
+"""
+Holds the class 'Dice' that represents a dice in the program
+"""
+
+import random
+
+class Dice:
+    """
+    Dice class that has all the information and functions necessary for a dice
+    """
+    sides: int
+    
+    DICE_TYPES = {4, 6, 8, 10, 12, 20, 100}
+    
+    def __init__(self, sides: int):
+        """
+        Checks if the new dice has a valid amount of sides and then creates the 
+        object
+        """
+        if sides not in self.DICE_TYPES:
+            raise ValueError(f"The a dice with a number of sides -> {sides} <- is not available")
+        else:
+            self.sides = sides
+            
+    def __str__(self):
+        return f"d{self.sides}"
+            
+    def roll(self) -> int:
+        """
+        Rolls a random number between 1 and the amount of sides of the dice and
+        returns the result
+        """
+        min_result = 1
+        max_result = self.sides
+        
+        return random.randint(min_result, max_result)
